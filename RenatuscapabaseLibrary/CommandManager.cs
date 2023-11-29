de72a375253e8761340310d98da826c7680ac408
Renatuscape <silarcta@gmail.com>
2023-11-29 15:04:25 +0100

    Renamed to command manager

---
+++ RenatuscapabaseLibrary/CommandManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using RenatuscapabaseLibrary.TableComponents;
+using System.Threading.Channels;
+
+namespace RenatuscapabaseLibrary
+{
+    public static class CommandManager
+    {
+        public static void Connect(string userCommand)
+        {
+            DbConnect dbConnect = new();
+
+            try
+            {
+                dbConnect.OpenConnection(out var command, out var connection);
+
+                if (userCommand == "0")
+                {
+                    SqlRepository.CreateTable(command);
+                }
+                else if (userCommand == "1")
+                {
+                    SqlRepository.DropTable(command);
+                }
+                else if (userCommand == "2")
+                {
+                    TableColumn newColumn = ColumnFactory.CreateColumn();
+                    Console.WriteLine("Which table would you like to add a column to?");
+                    string tableName = InputValidation.SanitiseName(Console.ReadLine() ?? "");
+                    SqlRepository.AddColumn(command, "TestTable", newColumn);
+                    Console.WriteLine(newColumn);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ RenatuscapabaseLibrary/DbConnect.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace RenatuscapabaseLibrary
+{
+    public class DbConnect
+    {
+        private string DataBaseName {  get; }
+        private string ServerAddress { get; }
+        private string ConnectionString { get; }
+
+        public DbConnect(string dataBaseName = "Renatuscapabase", string serverAddress = @"(localdb)\MSSQLLocalDB")
+        {
+            DataBaseName = dataBaseName;
+            ServerAddress = serverAddress;
+
+            ConnectionString = $"Server = {ServerAddress};";
+            ConnectionString += $"\nDatabase = {DataBaseName}\n;";
+            ConnectionString += @"Integrated Security = true;";
+        }
+        public SqlConnection CreateConnection()
+        {
+            return new(ConnectionString);
+        }
+
+        public void OpenConnection(out SqlCommand command, out SqlConnection connection)
+        {
+            connection = CreateConnection();
+            command = connection.CreateCommand();
+            connection.Open();
+        }
+    }
+}
