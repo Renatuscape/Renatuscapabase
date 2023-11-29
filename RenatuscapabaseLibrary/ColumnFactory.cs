9d0db751a34a589b65c77be9e168e4c45ea08abb
Renatuscape <silarcta@gmail.com>
2023-11-29 14:56:14 +0100

    Created column factory to create columns with user input

---
+++ RenatuscapabaseLibrary/ColumnFactory.cs
@@ -0,0 +1,62 @@
+﻿using RenatuscapabaseLibrary.TableComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenatuscapabaseLibrary
+{
+    public static class ColumnFactory
+    {
+        public static TableColumn CreateColumn()
+        {
+            ColumnDataType dataType = (ColumnDataType)ChooseDataType();
+            int dataLength = ChooseDataLength();
+            string columnName = ChooseColumnName();
+            bool isNullable = true;
+            bool isUnique = true;
+            string? content = null;
+
+            return new(dataType, dataLength, columnName, isNullable, isUnique, content);
+        }
+
+        static int ChooseDataType()
+        {
+            Console.WriteLine("Choose datatype");
+            int maxInt = Enum.GetNames(typeof(ColumnDataType)).Length;
+
+            for (int i = 0; i < maxInt; i++)
+            {
+                Console.WriteLine($"{Enum.GetNames(typeof(ColumnDataType))[i]}");
+                
+            }
+
+            int choice = -1;
+            while (choice < 0 || choice > maxInt)
+            {
+                Console.WriteLine("\t");
+                choice = Convert.ToInt32(Console.ReadKey().KeyChar.ToString());
+                if (choice < 0 || choice > maxInt)
+                {
+                    Console.WriteLine("Please choose a valid datatype.");
+                }
+            }
+
+            return choice;
+        }
+
+        static int ChooseDataLength()
+        {
+            Console.WriteLine("Please choose length of data.");
+            string input = Console.ReadLine() ?? "250";
+            return Convert.ToInt32(input);
+        }
+
+        static string ChooseColumnName()
+        {
+            Console.WriteLine("Enter column name. Only alphanumeric characters and underscore allowed.");
+            return InputValidation.SanitiseName(Console.ReadLine() ?? "NewColumn");
+        }
+    }
+}
