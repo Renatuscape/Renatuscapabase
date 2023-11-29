93454e4bbd7bb598be347d723f3f815503b25d97
Renatuscape <silarcta@gmail.com>
2023-11-29 15:03:56 +0100

    added static class to use for input validation.

---
+++ RenatuscapabaseLibrary/InputValidation.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RenatuscapabaseLibrary
+{
+    public static class InputValidation
+    {
+        public static string SanitiseName(string text)
+        {
+            return Regex.Replace(text, @"[^\w]", "");
+        }
+    }
+}
