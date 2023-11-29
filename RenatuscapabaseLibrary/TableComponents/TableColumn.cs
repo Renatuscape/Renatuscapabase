01a2e249fbfd943f2b4cd7ebfd45c7a65bd6a6a4
Renatuscape <silarcta@gmail.com>
2023-11-29 18:24:10 +0100

    Changed content to default content.

--- RenatuscapabaseLibrary/TableComponents/TableColumn.cs
+++ RenatuscapabaseLibrary/TableComponents/TableColumn.cs
@@ -5,41 +5,49 @@
         public ColumnDataType DataType { get; set; }
         public int DataLength { get; set; }
         public string ColumnName { get; set; }
-        public string? Content { get; set; }
-        public float NumericContent { get; set; }
+        public string? DefaultContent { get; set; }
+        public decimal NumericContent { get; set; }
         public bool IsNullable { get; set; }
         public bool IsUnique { get; set; }
 
-        public TableColumn(ColumnDataType dataType, int dataLength, string columnName, bool isNullable = false, bool isUnique = false, string? content = null)
+        public TableColumn(ColumnDataType dataType, int dataLength, string columnName, bool isNullable = false, bool isUnique = false, string? defaultContent = null)
         {
             DataType = dataType;
             DataLength = dataLength;
             ColumnName = columnName;
             IsNullable = isNullable;
             IsUnique = isUnique;
-            Content = content;
+            DefaultContent = defaultContent;
 
-            if (Content == null & IsNullable == false)
+            if (DefaultContent == null & IsNullable == false)
             {
                 throw new Exception("Content in this column cannot be null.");
             }
 
-            if (DataType == ColumnDataType.Decimal)
+            if (!string.IsNullOrWhiteSpace(DefaultContent))
             {
-                if (float.TryParse(content, out var parsedNumber))
+                if (DataType == ColumnDataType.Decimal)
                 {
-                    NumericContent = parsedNumber;
-                }
-                else
-                {
-                    throw new Exception("Parse failed. This column must have a numeric datatype.");
+                    if (decimal.TryParse(defaultContent, out var parsedNumber))
+                    {
+                        NumericContent = parsedNumber;
+                    }
+                    else
+                    {
+                        throw new Exception("Parse failed. This column must have a numeric datatype.");
+                    }
                 }
             }
+            else
+            {
+                defaultContent = null;
+            }
+
         }
 
         public override string ToString()
         {
-            return $"{ColumnName}: {Content} ({DataType} IS NULLABLE {IsNullable}, IS UNIQUE {IsUnique})";
+            return $"{ColumnName}: {DataType} IS NULLABLE {IsNullable}, IS UNIQUE {IsUnique}), {DefaultContent}";
         }
     }
 }
